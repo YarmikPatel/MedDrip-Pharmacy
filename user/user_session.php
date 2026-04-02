fb8b64f03dd6ab5a74fcb4aafcde37ea174863de
YarmikPatel <kansagarayarmik@gmail.com>
2026-04-03 01:24:49 +0530

    New project upload

--- user/user_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    //Check if user is logged in
-    if(!isset($_SESSION['uid'])){
-        echo 'user not logged in. UID not found in session';
-        header('location: ../login.php');
-    }
-?>
